27d1d71bc4df80a94a588ef449dd3305fc9756d1
Vinor <vinorval@yandex.ru>
2025-10-09 12:57:22 +0300

    fix: add new font-family

--- src/components/promo/Promo.js
+++ src/components/promo/Promo.js
@@ -6,30 +6,34 @@ const Promo = ({ timer }) => {
         <div className="invite" >
             <div className="invite__skroll-block">
                 <div className="invite__marryned">
-                    <p className="margin-none fw-600 fs-32" >Илья</p>
+                    <p className="margin-none fs-32 ff-philosopher" >Илья</p>
                     <img src={Heard} alt="heart" />
-                    <p className="margin-none fw-600 fs-32" >Лера</p>
+                    <p className="margin-none fs-32 ff-philosopher" >Лера</p>
                 </div>
                 <p className="margin-none invite__text" >Мы рады пригласить вас на торжество, которое станет одним из самых важных и запоминающихся событий в нашей жизни. Ваше присутствие сделает этот день еще более особенным и значимым для нас. Именно рядом с самыми близкими людьми мы хотим отпраздновать начало нового этапа нашей совместной жизни.</p>
             </div>
             <div className="invite__skroll-block">
-                <p className="invite__date margin-none fw-600 fs-20" >16 января 2026</p>
+                <div className="invite__date margin-none fw-400 fs-20" >
+                    <p className="margin-none ff-caveat fs-32">16 </p>
+                    <p className="margin-none">января </p>
+                    <p className="margin-none ff-caveat fs-32">2026</p>
+                </div>
                 <div className="timer">
                     <p className="timer__title margin-none fs-20" >До начала свадьбы осталось:</p>
                     <div>
-                        <p className="margin-none fw-600 fs-24" >{timer.days}</p>
+                        <p className="margin-none fw-400 fs-24 ff-caveat" >{timer.days}</p>
                         <p className="margin-none fs-20" >дней</p>
                     </div>
                     <div>
-                        <p className="margin-none fw-600 fs-24" >{timer.hours}</p>
+                        <p className="margin-none fw-400 fs-24 ff-caveat" >{timer.hours}</p>
                         <p className="margin-none fs-20" >часов</p>
                     </div>
                     <div>
-                        <p className="margin-none fw-600 fs-24" >{timer.minutes}</p>
+                        <p className="margin-none fw-400 fs-24 ff-caveat" >{timer.minutes}</p>
                         <p className="margin-none fs-20" >минут</p>
                     </div>
                     <div>
-                        <p className="margin-none fw-600 fs-24" >{timer.seconds}</p>
+                        <p className="margin-none fw-400 fs-24 ff-caveat" >{timer.seconds}</p>
                         <p className="margin-none fs-20" >секунд</p>
                     </div>
                 </div>
